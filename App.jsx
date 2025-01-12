f8d37d8a96f90b56268f8247de5e1b272dd62ab7
Shrayash_Gawade_17 <115636605+shrayash17@users.noreply.github.com>
2025-01-12 13:32:28 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,39 +1,47 @@
-import { useState , useEffect} from 'react'
+import { useState,lazy, Suspense } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import axios from 'axios'
+import { BrowserRouter, Route, Routes, useNavigate, } from 'react-router-dom'
+const Dashboard= lazy(()=> import("./components/Dashboard"))
+const Landing = lazy(()=>import("./components/Landing"))
 
 function App() {
-  const [todos,setTodos]=useState([])
-
-  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then((res)=>{
-      setTodos(res.data)
-    })
-
-
-  },[])
-
 
   return (
     <>
-      {todos.map(td=> <Todo {...td}/>)}
+    
+     <BrowserRouter>
+     <Appbar/>
+     <Suspense fallback={<div>Loading ..</div>}>
+     <Routes>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/' element={<Landing/>}/>
+     </Routes>
+     </Suspense>
+     
+     </BrowserRouter>
     </>
   )
 }
 
-function Todo({userId, id, title, body}){
-  return(
+function Appbar(){
+  
+  const navigate= useNavigate();
+  return (
     <>
-    <h1>User ID: {userId}</h1>
-    <h2>ID: {id}</h2>
-    <h3>Title: {title}</h3>
-    <p>{body}</p>
-    
-    </>
+    <div>
+
+<button onClick={()=>{
+  navigate("/")
+}}>Landing Page</button>
+<button onClick={()=>{
+navigate("/dashboard")      }}>Dashboard</button>
+</div></>
   )
+  
+
+
 }
 
 export default App
